a58c125f7db9239aff95be2e0fa1f28bcc5bb3ec
Derek Shen <derekbshen@gmail.com>
2026-07-27 11:22:15 -0400

    Initial Commit

---
+++ FreshTrack/FreshTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshTrackApp.swift
+//  FreshTrack
+//
+//  Created by Derek Shen on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FreshTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
